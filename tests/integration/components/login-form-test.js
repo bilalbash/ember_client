e7dee7135b62e6a2560c7517a571fa2f7dc1a6b1
bilalbash <bilal.basharat@gmail.com>
2016-07-11 07:02:02 +0500

    ember client init

---
+++ tests/integration/components/login-form-test.js
@@ -0,0 +1,31 @@
+// import { moduleForComponent, test } from 'ember-qunit';
+// import hbs from 'htmlbars-inline-precompile';
+// 
+// moduleForComponent('login-form', 'Integration | Component | login form', {
+//   integration: true
+// });
+
+// test('it renders', function(assert) {
+//   // Set any properties with this.set('myProperty', 'value');
+//   // Handle any actions with this.on('myAction', function(val) { ... });
+// 
+//   this.render(hbs`{{login-form}}`);
+// 
+//   assert.equal(this.$().text().trim(), '');
+// // 
+// //   // Template block usage:
+// //   this.render(hbs`
+// //     {{#login-form}}
+// //       template block text
+// //     {{/login-form}}
+// //   `);
+// // 
+// //   //assert.equal(this.$().text().trim(), "Login");
+// //   //console.log(this.$());
+// //   //console.log(this);
+// //   //alert(this.$());
+// //   //alert(this);
+// //   //alert($(this).find('[for="identification"]').text());
+// //   alert(this.$().find('[for="identification"]').text());
+// //   assert.equal(this.$().find('[for="identification"]').text(), "Login");
+// });
